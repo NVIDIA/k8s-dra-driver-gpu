da46b956a5797f8d32f20e59b358f7f1066bfebe
Nour <nurmn3m@gmail.com>
2025-10-20 22:04:56 +0300

    Adding usr/lib to paths for gpu-kubelet and compute-domain
    Signed-off-by: Nour <nurmn3m@gmail.com>

--- cmd/compute-domain-kubelet-plugin/root.go
+++ cmd/compute-domain-kubelet-plugin/root.go
@@ -34,6 +34,7 @@ func (r root) getDriverLibraryPath() (string, error) {
 		"/lib64",
 		"/lib/x86_64-linux-gnu",
 		"/lib/aarch64-linux-gnu",
+		"/usr/lib",
 	}
 
 	libraryPath, err := r.findFile("libnvidia-ml.so.1", librarySearchPaths...)
